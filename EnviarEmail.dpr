8b57aeed111caaec6614dcdbbc0e794174f977b8
TDevRocks <tdevrocks@tdevrocks.com.br>
2017-05-05 14:58:17 -0300

    Envio de Email

---
+++ EnviarEmail.dpr
@@ -0,0 +1,14 @@
+program EnviarEmail;
+
+uses
+  Vcl.Forms,
+  UntPrincipal in 'UntPrincipal.pas' {frmPrincipal};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TfrmPrincipal, frmPrincipal);
+  Application.Run;
+end.
